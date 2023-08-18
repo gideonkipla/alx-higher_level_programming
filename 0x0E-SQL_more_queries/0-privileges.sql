511f29cb5afa27dbbee3d1d0374766aa71416d4f
gideonkipla <monseignuergk@gmail.com>
2023-08-17 19:12:59 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
