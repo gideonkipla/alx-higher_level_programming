419e33fce230a5462f688bd7281432d266745376
gideonkipla <monseignuergk@gmail.com>
2023-10-24 15:42:46 -0700

    javascript-web_scraping

--- 0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines an object attribute and methods using lookup function."""
-
-
-def lookup(obj):
-    """Return a list of an object's available attributes and methods."""
-    return (dir(obj))
